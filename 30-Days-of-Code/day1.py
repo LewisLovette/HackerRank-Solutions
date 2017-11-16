e6595d1af6619fe4da284b11251667e421781b88
LewisLovette <lewislovette3@hotmail.co.uk>
2017-11-16 22:59:19 +0000

    Should all be updated now, will try to keep up to date instead of mass adding...

---
+++ 30-Days-of-Code/day1.py
@@ -0,0 +1,15 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+ii = int(input())
+dd = eval(input())
+ss = input()
+# Print the sum of both integer variables on a new line.
+sumOfInt = i + ii
+sumOfDouble = d + dd
+sumOfString = s + ss
+# Print the sum of the double variables on a new line.
+print(str(sumOfInt)+"\n"+str(sumOfDouble)+"\n"+sumOfString)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
