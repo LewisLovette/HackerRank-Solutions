100256703513b20ca6e879da70681aa2c292e760
lovettel <lewislovette3@hotmail.co.uk>
2018-03-08 16:59:14 +0000

    Feel more comfy with try/catch but not sure about error names yet

---
+++ 30-Days-of-Code/day16.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    
+    try{
+        int temp = stoi(S);
+        cout << temp << endl;
+    } catch(...){	//catch all, should find specific error generally.
+        cout << "Bad String";
+    }
+    
+    return 0;
+}
