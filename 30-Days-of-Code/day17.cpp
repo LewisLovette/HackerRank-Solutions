84f1140ae3412660987b32b034e34657af31f1e8
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-03-09 15:40:44 +0000

    Turns out I didn't submit
    so here it is -_-

--- 30-Days-of-Code/day17.cpp
+++ 30-Days-of-Code/day17.cpp
@@ -5,6 +5,8 @@ class Calculator{
             int pow = n;
             if(n < 0 or p < 0){
                 throw runtime_error("n and p should be non-negative");
+            } else if(p == 0){
+                return 1;   //as anything **0 = 1
             }
 
             for(int i = 0; i < p-1; i++){//-1 as first pow is == original
