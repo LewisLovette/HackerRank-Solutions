be0c362f2dae230e530634304a645c867d5954f8
lovettel <lewislovette3@hotmail.co.uk>
2018-03-12 13:36:35 +0000

    wanted a quick one since i'm stuck on insersion sort at the momet, ah well

--- C++/Classes/exceptionalServer.cpp
+++ C++/Classes/exceptionalServer.cpp
@@ -1,16 +1,12 @@
-try
-{
+try{
     cout << Server::compute(A,B) << endl;
 }
-catch(bad_alloc& error)
-{
+catch(bad_alloc& error){
     cout << "Not enough memory" << endl;
 }
-catch(exception& error)
-{
+catch(exception& error){
     cout << "Exception: "<< error.what() << endl;
 }
-catch(...)
-{
+catch(...){
     cout << "Other Exception" << endl;
 }
