3d20497dbdfd22276e4fbb11d8653c0fec900c71
lovettel <lewislovette3@hotmail.co.uk>
2018-03-18 12:19:47 +0000

    Finished up this sorting algorithm, a few annoying errors but somewhat simple when you go step by step.

---
+++ Algorithms/Sorting/insersionSort-Part2.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int size, temp;  //set to 2 for first as always search
+    cin >> size;
+    vector<int> vec;
+    
+    //input to vec
+    for(int i = 0; i < size; i++){
+        cin >> temp;
+        vec.push_back(temp);
+    }
+    //For Insersion Sort - Finds unsorted number, moves larger numbers ahead +1 (when true) then places in pos (when not true)
+    for(int i = 1; i < size; i++){
+        if(vec[i] < vec[i-1]){  //Means the number is unsorted
+            temp = vec[i];  //first swap as condition is true
+            vec[i] = vec[i-1];
+            for(int j = i; j > 0; j--){
+                if(temp < vec[j] and temp > vec[j-1]){    //First run will be the number we're sorting
+                    vec[j] = temp;
+                    break;
+                }else if(j == 1 and temp < vec[0]){ //For when first position
+                    vec[1] = vec[0];
+                    vec[0] = temp;
+                }else{  //If no condition is met, will move
+                    vec[j] = vec[j-1];
+                }
+            }
+            if(i == 1 and temp < vec[i]){   //For swapping 2nd with 1st position
+                vec[0] = temp;
+            }
+        }
+        //Printing
+        for(int i = 0; i < size; i++) cout << vec[i] << " ";
+        cout << endl;
+    }
+    
+    return 0;
+}
