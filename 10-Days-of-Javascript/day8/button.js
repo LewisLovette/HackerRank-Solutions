bb2291e83d76d2d12ce0dcd07f58617cb3a74698
lovettel <lewislovette3@hotmail.co.uk>
2018-03-25 13:42:58 +0100

    1 More to go for 10-Days-of-Javascript! This was a fun one.

--- 10-Days-of-Javascript/day8/button.js
+++ 10-Days-of-Javascript/day8/button.js
@@ -1,8 +1,8 @@
 var countClicks = 0, btn = document.getElementById('btn');  //sets up button to btn
-btn.className = "btn";  //so works with CSS and HTML for the style in css and event listener in this
-//btn.style.css = "width: 96px; height: 48px; font-size: 24px;"
+btn.className = "btn";
+btn.innerHTML = countClicks;
 
-btn.addEventListener('click',function(){ 
+btn.addEventListener("click",function() {
     countClicks++;
-    btn.innerHTML = countClicks;
-})
+    this.innerHTML = countClicks;
+});
