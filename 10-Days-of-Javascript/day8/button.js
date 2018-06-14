e3ebba18931d51aff9f902bb1b4eb57b0fbc422b
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-06-14 12:32:18 +0100

    Working solution...
    worked before but wasn't accepted, for whatever reason it liked this one more.

--- 10-Days-of-Javascript/day8/button.js
+++ 10-Days-of-Javascript/day8/button.js
@@ -1,8 +1,3 @@
-var countClicks = 0, btn = document.getElementById('btn');  //sets up button to btn
-btn.className = "btn";
-btn.innerHTML = countClicks;
-
-btn.addEventListener("click",function() {
-    countClicks++;
-    this.innerHTML = countClicks;
-});
+function increase(btn){
+    btn.textContent = 1 + Number(btn.textContent);
+}
