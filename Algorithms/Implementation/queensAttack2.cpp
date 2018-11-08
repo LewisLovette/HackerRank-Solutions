c8ecedb6cfb0fc9edb6219aaa2cfa681491d87ac
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-11-08 09:39:03 +0000

    Setup - not completed
    Just need to check if position is occupied and break loop

---
+++ Algorithms/Implementation/queensAttack2.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+
+
+//used to check across the 8 directions of a queen to see other queens
+bool queenInteract(int board[8][8], int x, int y){
+    //if true, interaction has happened
+
+    int xC, yC;
+
+    //verticle
+    for(int i = 0; i < 8; i++){
+        if(board[i][y] == 1) return true;
+    }
+    //horizontal
+    for(int i = 0; i < 8; i++){
+        if(board[x][i] == 1) return true;
+    }
+
+    //up left
+    xC = x, yC = y; //xCopy, yCopy
+    while(xC >= 0 and yC >= 0){ //set to 8 as max possible moves
+        if(board[xC][yC] == 1) return true;
+        xC -= 1;
+        yC -= 1;
+    }
+    //down right
+    xC = x, yC = y; //xCopy, yCopy
+    while(xC <= 7 and yC <= 7){ //set to 8 as max possible moves
+        if(board[xC][yC] == 1) return true;
+        xC += 1;
+        yC += 1;
+    }
+
+    //up right
+    xC = x, yC = y; //xCopy, yCopy
+    while(xC >= 0 and yC <= 7){ //set to 8 as max possible moves
+        if(board[xC][yC] == 1) return true;
+        xC -= 1;
+        yC += 1;
+    }
+    //down left
+    xC = x, yC = y; //xCopy, yCopy
+    while(xC <= 7 and yC >= 0){ //set to 8 as max possible moves
+        if(board[xC][yC] == 1) return true;
+        xC += 1;
+        yC -= 1;
+    }
+
+    return false;
+}
+
+int main(){
+    int board[8][8] = {0}; //initialize board positions to 0
+    //board[0][0] = 1;
+    //vector<int> columnFull;
+    bool temp = false;
+
+    //placing queens
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            //if(find(columnFUll.begin(), columnFull.end(), y) != columnFull.end()) continue;   //this column has a queen in it
+            temp = queenInteract(board, i, j);
+            if(temp == false){   //if no interaction then place queen and move to next line
+                board[i][j] = 1;
+                //columnFull.push_back(y);  //column has a queen in it
+                //break; //as can't do more on this row
+            }
+            for(int x = 0; x < 8; x++){
+                for(int y = 0; y < 8; y++) cout << board[x][y] << " ";
+                cout << endl;
+            }
+            cout << endl;
+        }
+    }
+
+    for(int x = 0; x < 8; x++){
+        for(int y = 0; y < 8; y++) cout << board[x][y] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
