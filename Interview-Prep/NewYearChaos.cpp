830050b89d826a9749938d9e7f093b8ec78a3b02
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-05-09 16:56:17 +0100

    new year chaos - a touch one

---
+++ Interview-Prep/NewYearChaos.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumBribes function below.
+void minimumBribes(vector<int> q) {
+    int result = 0;
+
+    for(long i = (long)q.size() - 1; i >= 0; i--){
+        //2 Qs, how much has 1 person moved forward, how much has someone been bribed
+       if(q[i] - (i+1) > 2){  //1-2 bribes
+            cout << "Too chaotic" << "\n";
+            return;
+        }
+        for(long j = max(0, q[i] - 2); j < i; j++)  //2 bribes max so start 2 ahead
+            if(q[j] > q[i]) result++;
+    }
+
+    cout << result << "\n";
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string q_temp_temp;
+        getline(cin, q_temp_temp);
+
+        vector<string> q_temp = split_string(q_temp_temp);
+
+        vector<int> q(n);
+
+        for (int i = 0; i < n; i++) {
+            int q_item = stoi(q_temp[i]);
+
+            q[i] = q_item;
+        }
+
+        minimumBribes(q);
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
